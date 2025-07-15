7a567438380c5a3a6b7da10995c71700b868a5ce
kitherain <kitherain@gmail.com>
2025-07-14 21:24:37 -0700

    up and running

--- main.py
+++ main.py
@@ -5,3 +5,39 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
+token = os.getenv('DISCORD_TOKEN')
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+
+intents = discord.Intents.default()
+
+intents.message_content = True
+
+intents.members = True
+
+bot = commands.Bot(command_prefix='/', intents = intents)
+
+
+
+@bot.event
+async def on_ready():
+    print(f"on_ready called {bot.user.name}")
+
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+    if "shunzo" in message.content.lower():
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} counted")
+    
+    await bot.process_commands(message)
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hello {ctx.author.mention}!")
+
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
