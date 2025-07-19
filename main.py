79a0c7d8c81ce3431db34e7ef8acbe01237899ba
kitherain <kitherain@gmail.com>
2025-07-19 08:08:35 -0700

    add reaction

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ import logging
 from dotenv import load_dotenv
 import os
 import webserver 
-from datetime import datetime
+import datetime
 import ast
+from pytz import timezone, utc
+import random
+
+
 load_dotenv()
 
 token = os.getenv('DISCORD_TOKEN')
@@ -14,6 +18,7 @@ secret = os.getenv('SECRET')
 directory = ast.literal_eval(os.getenv('DIRECTORY'))
 available = os.getenv('AVAILABLE') == True
 owner = ast.literal_eval(os.getenv('OWNER'))
+
 
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
@@ -25,6 +30,7 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='/', intents = intents)
 
+pings = []
 
 
 @bot.event
@@ -38,20 +44,38 @@ async def on_message(message):
         return
     
     reply = ""
-    
-    if datetime.now().strftime('%m-%d') == bday:
-        reply += "Thanks " + directory[message.author.id] + "!" 
+    for m in message.mentions:
+        if m.id == owner:
+            pings.append(message.created_at)
+            if not available:
+                reply += "I don't think he's available right now."
+            break
 
+
+    date = datetime.datetime.now(tz=utc)
+    date = date.astimezone(timezone('US/Pacific'))
+    if date.strftime('%m-%d') == bday:
+        if "happy birthday" in message.content.lower() or "hbd" in message.content.lower:
+            await message.add_reaction(str('❤️'))
+            if reply == "":
+                choice = random.randint(0,1)
+                if choice == 0:
+                    reply += "Thanks " + directory[message.author.id] + "!" 
+                elif choice == 1:
+                    reply += "tysm " + directory[message.author.id] + "!"
+            else:
+                choice = random.randint(0,1)
+                if choice == 0:
+                    reply += "I'm sure he's grateful though."
+                elif choice == 1:
+                    reply += "I'll pass it on. Thanks!"
+    
     if secret in message.content.lower():
         await message.delete()
         await message.channel.send(f"{message.author.mention} counted!")
 
-    for m in message.mentions:
-        if m.id == owner:
-            await message.channel.send(f"pinged{message.author}")
 
     await message.channel.send(reply)
-
     await bot.process_commands(message)
 
 @bot.command()
